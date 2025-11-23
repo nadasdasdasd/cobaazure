f81a68a479d1bbd94a48a9558308dbe491b1dd30
Nada.NisrinaSeptiana <nada.nisrinaseptiana@homecredit.co.id>
2025-11-23 15:40:54 +0700

    feat: fix  env

--- src/modules/env/env.js
+++ src/modules/env/env.js
@@ -6,14 +6,10 @@ function isBrowser() {
 
 export default function env(key = '') {
   const prefix = (isBrowser() ? window.__ENV['REACT_ENV_PREFIX'] : process.env.REACT_ENV_PREFIX) || 'REACT_APP'
-  console.log("prefix", prefix)
-  console.log("isBrowser", isBrowser())
   const safeKey = `${prefix}_${key}`
-  console.log("safeKey", safeKey)
 
 
   if (isBrowser()) {
-    console.log("window.__ENV[safeKey]", window.__ENV[safeKey])
     return window.__ENV[safeKey]
   }
 
